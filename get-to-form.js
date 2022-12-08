b31503985bce356ff4d5a3b1a754963cdef56a4d
Baptcave <[noye.baptiste@orange.fr](mailto:noye.baptiste@orange.fr)>
2022-12-08 22:02:00 +0100

    added script links

---
+++ get-to-form.js
@@ -0,0 +1,10 @@
+// const imageLink = document.querySelector(".image-link");
+// const projectName = document.querySelector(".h3-team");
+// const formName = document.querySelector(".form-name");
+// const memberPicture = document.querySelector(".member-picture");
+
+// // projectName.addEventListener('click', 
+// //     function() {
+// //         projectName.style.color = "red";
+// //     }
+// // );
