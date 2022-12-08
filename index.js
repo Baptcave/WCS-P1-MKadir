2649a6d5b659abc4c9e4058220da20f388ab3b58
Baptcave <[noye.baptiste@orange.fr](mailto:noye.baptiste@orange.fr)>
2022-12-08 11:53:14 +0100

    Revert "tests que je vais revert"
    This reverts commit 802a9790a19e37102d040d34bd6e9425935bf648.

--- index.js
+++ index.js
@@ -2,13 +2,3 @@ import { w3cHtmlValidator } from 'w3c-html-validator';
 
 const options = { filename: 'docs/index.html' };
 w3cHtmlValidator.validate(options).then(w3cHtmlValidator.reporter);
-
-const memberPic = document.querySelector(".member-picture");
-const projectName = document.querySelector(".h3-team");
-
-const formName = document.querySelector(".form-name");
-
-memberPic.addEventListener("click", function (event) {
-    const profile = memberPic.parentNode.projectName.innerText;
-    console.log(profile);
-});
